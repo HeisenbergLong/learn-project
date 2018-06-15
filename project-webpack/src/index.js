385d932f0b8a63ed84ad7bdc45896ce5bf98eefd
David <525065478@qq.com>
2018-06-15 19:58:56 +0800

    webpack

--- project-webpack/src/index.js
+++ project-webpack/src/index.js
@@ -1,5 +1,22 @@
-function index(){
-    console.log('index.........')
+import _ from 'lodash';
+import './style.css';
+import ico from './ico.png';
+import $ from '../bower_components/jquery/dist/jquery.min.js';
+
+function component() {
+  var element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  var myIcon = new Image();
+  myIcon.src = ico;
+
+  element.appendChild(myIcon);
+
+  return element;
 }
 
-index();
+document.body.appendChild(component());
